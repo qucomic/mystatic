e4bf3cf41860b60ee3b8330e94f6c4eddf7cc8fd
qucomic <qucomic@gmail.com>
2025-09-22 13:05:40 +0800

    add static

--- qimanwap_static/js/decode.js
+++
@@ -1,5 +0,0 @@
-var images=window.atob(image_urls).split(',');
-for(var i in images){
-    var html = "<li class=\"comic-page\"><img referrerpolicy=\"no-referrer\" class=\"lazydetail\" data-original=\"" + images[i] + "\" src=\"https://jmzj.me/static/images/loading.jpg\"></li>"
-    $(".comic-list").append(html);
-}
